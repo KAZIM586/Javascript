3525d91a93ff51049c9fd119c5f9df6ea831c904
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-07 23:45:43 +0530

    Ternary operators

---
+++ ternaryOperator.js
@@ -0,0 +1,12 @@
+// Question:1. Write a program that takes in a number and outputs whether it is positive, negative, or zero.
+
+// Ans:1-
+
+let num = 5;
+num == 0
+? console.log("Number is zero")
+: num > 0
+? console.log("Number is positive")
+: console.log("Number is negative");
+
+// OUTPUT:- Number is positive
