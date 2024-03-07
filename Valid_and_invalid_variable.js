71ba0f0a4f24596cc59f1830c6dfd44ef5c8a36c
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-07 23:44:57 +0530

    Valid and invalid variable in javascript

---
+++ Valid_and_invalid_variable.js
@@ -0,0 +1,18 @@
+// Questions-2: Create 2 valid variables and 2 invalid variables and print them onto the console. Comment the results and error messages.
+
+// Ans:2
+
+
+// VALID VARIABLE
+
+let str = "This is string";
+console.log(str)    // OUTPUT: This is string
+
+
+// INVALID STRING
+
+let const = "Mohan";
+console.log(const)   // OUTPUT :- SyntaxError: Unexpected token 'const'
+
+let 1num = 25;
+console,log(1num);   //OUTPUT :- SyntaxError: invalid or unexpected token
