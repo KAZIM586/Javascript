760f09daf1c1be8c51945ba96313b93908966c7a
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-07 23:46:25 +0530

    Ternary Operator2 problem

---
+++ TernaryOperator2.js
@@ -0,0 +1,14 @@
+// Question:2. Create a program that takes in two numbers and prints the larger one.
+
+// Ans:
+
+let num1 = 10;
+let num2 = 15;
+num1 == num2
+? console.log(`Both the number are equal`)
+: num1 > num2
+? console.log(`The larger number among the two numbers is ${num1}`)
+: console.log(`The larger number among the two numbers is ${num2}`)
+
+
+// OUTPUT:- The larger number among the two numbers is 15
