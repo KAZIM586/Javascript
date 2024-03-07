2ffb374fcd9cc34078c2bf33a0091dd80f29f008
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-07 23:41:41 +0530

    Arithmetic operators

---
+++ Arithmetic_Opertors.js
@@ -0,0 +1,33 @@
+//QUESTION:- 2. Write a program to perform all the arithmetic operations[except increment and decrement operators] of javascript of any two numbers stored in the variables num1 and num2. Also, print the results to the console.
+
+// Ans:2-
+let num1 = 8;
+let num2 = 3;
+
+// ADDITION
+console.log(`The addition of num1 and num2 is : ${num1 + num2}`)
+
+// Suntraction
+console.log(`The subtraction of num1 and num2 is : ${num1 - num2}`)
+
+// Multiplication
+console.log(`The multiplication of num1 and num2 is : ${num1 * num2}`)
+
+// Division
+console.log(`The division of num1 and num2 is : ${num1 / num2}`)
+
+// Modulus(%) returns the remainder
+console.log(`The modulo operation of num1 and num2 is : ${num1 % num2}`)
+
+// Exponentiation(**) raise the power
+console.log(`The exponential of num1 and num2 is : ${num1 ** num2}`)
+
+
+// OUTPUT:
+
+// The addition of num1 and num2 is : 11
+// The subtration of num1 and num2 is : 5
+// The multiplication of num1 and num2 is : 24
+// The division of num1 and num2 is : 2.6666666666666665
+// The modulo operation of num1 and num2 is : 2
+// The exponential of num1 and num2 is : 512
