0fb4b7f8df04829e315410ce87446217f7dd2c1d
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-07 23:44:00 +0530

    Comparison operators

---
+++ Comparition_Operator.js
@@ -0,0 +1,84 @@
+// 4. Write a program to demonstrate the results of comparison operators. Note that both the truth and false condition for each operator must be specified.
+
+
+// Ans:4-
+
+// Equal
+
+let num1 = 5;
+let num2 = 6;
+console.log(num1 == num2)  // OUTPUT: false
+
+let num3 = 6;
+let num4 = 6;
+console.log(num3 == num4)  // OUTPUT: true
+
+// Not Equal
+
+let num5 = 9;
+let num6 = 9;
+console.log(num5 != num6)  // OUTPUT: false
+
+let num7 = 3;
+let num8 = 2;
+console.log(num7 != num8)  // OUTPUT: true
+
+// Strictly Equal
+
+let num9 = 9;
+let num10 = 9;
+console.log(num9 === num10)  // OUTPUT: true
+
+let num11 = 3;
+let num12 = 2;
+console.log(num11 === num12)  // OUTPUT: false
+
+// Stricly Not Equal
+
+let num13 = 9;
+let num14 = 9;
+console.log(num13 !== num14)  // OUTPUT: false
+
+let num15 = 3;
+let num16 = 2;
+console.log(num15 !== num16)  // OUTPUT: true
+
+// Greater then
+
+let num17 = 20;
+let num18 = 9;
+console.log(num17 > num18)  // OUTPUT: true
+
+let num19 = 3;
+let num20 = 21;
+console.log(num19 > num20)  // OUTPUT: false
+
+// Greater then or Equal to
+
+let num21 = 20;
+let num22 = 9;
+console.log(num21 >= num22)  // OUTPUT: true
+
+let num23 = 3;
+let num24 = 21;
+console.log(num23 >= num24)  // OUTPUT: false
+
+// Less then
+
+let num25 = 2;
+let num26 = 9;
+console.log(num25 < num26)  // OUTPUT: true
+
+let num27 = 30;
+let num28 = 21;
+console.log(num27 < num28)  // OUTPUT: false
+
+// Less then or Equal to
+
+let num29 = 2;
+let num30 = 9;
+console.log(num29 <= num30)  // OUTPUT: true
+
+let num31 = 30;
+let num32 = 21;
+console.log(num31 <= num32)  // OUTPUT: false
