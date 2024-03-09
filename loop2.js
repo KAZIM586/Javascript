a41e27b1d70063d69ac6345ecaca5db0e9348a69
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-09 22:06:59 +0530

    Add files via upload

---
+++ loop2.js
@@ -0,0 +1,23 @@
+// Question:2- Write a program that prints all the positive even numbers till the number specified.
+
+// Ans:2
+
+let number = 20;
+for (i = 1; i<=number; i++) {
+if( i % 2 == 0){
+    console.log(i);
+}
+}
+
+
+// OUTPUT:-
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+// 20
