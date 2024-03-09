23d223d937107659d23c9d55348215a094c6e958
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-09 22:06:39 +0530

    table problem

---
+++ loopMultiplicationTable.js
@@ -0,0 +1,20 @@
+// Question:1. Write a program that generates the multiplication table in the textbook format for a given number.
+
+// Ans:1
+
+let number = 3;
+for(i=1; i<=10;i++){
+    console.log(`${number} X ${i} = ${number * i}`)
+}
+
+// OUTPUT:-
+// 3 X 1 = 3
+// 3 X 2 = 6
+// 3 X 3 = 9
+// 3 X 4 = 12
+// 3 X 5 = 15
+// 3 X 6 = 18
+// 3 X 7 = 21
+// 3 X 8 = 24
+// 3 X 9 = 27
+// 3 X 10 = 30
