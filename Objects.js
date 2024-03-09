9a35d862450fdb70d97b8c630e8825d35629c49b
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-09 22:05:55 +0530

    objects problem in javascript

---
+++ Objects.js
@@ -0,0 +1,12 @@
+// Question:3. Create an object of a student registry of 5 students whose key is the registration number and the value is the student name. Registration number starts from 1 and continues.
+
+// Ans:
+
+let obj = {
+    1: "Monu",
+    2: "Sonu",
+    3: "Rohit",
+    4: "Rajan",
+    5: "Rinki",
+};
+console.log(obj);
