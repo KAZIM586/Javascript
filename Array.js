68395792b59eb5aeb046383dfe76fe6235d8b883
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-09 22:07:54 +0530

    Array problem in js

---
+++ Array.js
@@ -0,0 +1,5 @@
+
+// 2. Create an array of 10 products that you have recently purchased or viewed on an e-commerce site.
+
+let arr = ["Mobile","Laptop","Bag","Trimmer","Mouse","Keyboard","Neckband","Laptop Battery","Mouse Pad","Headphone"];
+console.log(arr);
