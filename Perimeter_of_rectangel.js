9ca8bed3e43c88568080bb2d3fa2f2a0cb2edd07
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-11 23:01:26 +0530

    perimeter problem

---
+++ Perimeter_of_rectangel.js
@@ -0,0 +1,12 @@
+// QUESTION:-3. Write a program to find out the perimeter of a rectangle. Print the results to the console.
+// Ans:3
+
+let length = 5;
+let width = 6;
+
+perimeter = 2 * (length * width)
+
+console.log(`The perimeter of rectangle is : `,perimeter)
+
+
+// OUTPUT:  The perimeter of rectangle is :  60
