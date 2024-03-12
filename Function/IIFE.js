3f76c075eefb3359f93b1bd8c6a05a71c1c741e3
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-12 22:54:25 +0530

    Immediately invoked function expression program

---
+++ Function/IIFE.js
@@ -0,0 +1,11 @@
+// IIFE Immediately Invoked Function Expression
+(function message(){
+    console.log(`Message hai `);
+})();
+
+
+
+((name) =>{
+    console.log(`Apna kam kar ${name} ji .`);
+})("Sonu");
+
