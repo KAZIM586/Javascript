ee16aa7f07fd65b9c843de0111c3f00ae16305a8
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-15 21:27:56 +0530

    Object Function program

---
+++ Function/objectFunction.js
@@ -0,0 +1,10 @@
+const user={
+    username:"Mohit",
+    address:"Lucknow"
+}
+
+function handleObject(anyObject){
+console.log(`Username is ${anyObject.username} and address is ${anyObject.address}`);
+}
+
+handleObject(user)
