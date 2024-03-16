350f01b24efd2592564c564affe966a87ef181c6
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-16 22:51:40 +0530

    For of loop program

---
+++ Loop/forof.js
@@ -0,0 +1,19 @@
+// For of
+
+const arr = [1,2,3,4,56,]
+for (const num of arr) {
+    // console.log(num);
+}
+
+
+const greetings ="Hello world"
+for(const greet of greetings){
+// console.log(greet);
+}
+
+
+
+
+
+
+
