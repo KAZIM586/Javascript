a50343f09092f2fad5f6dd1a25440a35f3f28aaa
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-16 22:50:47 +0530

    For in loop

---
+++ Loop/forInLoop.js
@@ -0,0 +1,13 @@
+// Object by for in loop
+
+const myObject={
+    name:"Mohit",
+    address:"Lucknow",
+    phoneNumber:9578985123
+
+}
+
+for (const key  in myObject) {
+    // console.log(myObject[key] );
+    
+}
