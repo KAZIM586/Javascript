2d1eeb1a4efb44ddb96e284b871cadd1b710436b
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-16 22:50:06 +0530

    For each loop program

---
+++ Loop/forEachLoop.js
@@ -0,0 +1,26 @@
+// for each loop
+
+const arr2 =["mohan", "sohan","rohit","mohit"]
+arr2.forEach(element => {
+    // console.log(element);
+});
+
+
+const coding =[
+    {
+    languageName :"Python",
+    languageFileName : "py"
+    },
+    {
+    languageName :"Java",
+    languageFileName : "java"
+    },
+    {
+    languageName :"JavaScript",
+    languageFileName : "s"
+    },
+]
+
+coding.forEach((item) => {
+console.log(item);
+})
