c549d3d23ede4e85a9720d6ad7a9a0a8be611817
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-18 21:58:03 +0530

    map program in js

---
+++ Loop/map.js
@@ -0,0 +1,11 @@
+// Map
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+// console.log(map);
+for (const [key , value] of map) {
+    // console.log(key, ':-' , value );
+    
+}
